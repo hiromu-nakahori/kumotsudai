94b66157cb98737f82e3ae8462c3fa7b62ddfb39
hatyibei <hatyibei@yahoo.co.jp>
2025-08-07 14:43:34 +0900

    bilidmissing

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,3 +1,12 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { helloWorld } from './function/hello-world/resource';
 
-defineBackend({});
+const backend = defineBackend({
+    helloWorld,
+})
+
+backend.addOutput({
+    custom: {
+        helloWorldFuctionName: backend. helloWorld. resources. lambda. functionName
+    },
+});
